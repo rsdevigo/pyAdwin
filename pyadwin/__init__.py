c5de199d9c7762d87836035b4763c4f597d56957
Sanne de Roever <sanne.de.roever@gmail.com>
2016-11-01 17:15:20 +0100

    Changed util reference into pyadwin, changed the test to trigger window changes, fixed some warnings

--- pyadwin/__init__.py
+++ pyadwin/__init__.py
@@ -1,3 +1,4 @@
-from pyadwin.ad_win_list_node import AdWinListNode
 from pyadwin.ad_win_list import AdWinList
+from pyadwin.ad_win_list_node import AdWinListNode
 from pyadwin.adwin import Adwin
+
--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from pyadwin import Adwin
 
-#Delta's standard of 0.01 , but Adwin builder can receive a floating point as the delta parameter.
-adwin = Adwin()
+# Delta's standard of 0.01 , but Adwin builder can receive a floating point as the delta parameter.
+adwin = Adwin(0.01)
 
-data_stream = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.7]
+data_stream = [1] * 30
+data_stream.extend([2] * 15)
 
 for data in data_stream:
-  if (adwin.update(data)):
-    print "Change has been detected in data: "+str(data)
-  print adwin.getEstimation() # Prints the next value of the estimated form of data_stream
+    if (adwin.update(data)):
+        print "Change has been detected in data: " + str(data)
+    print adwin.getEstimation()  # Prints the next value of the estimated form of data_stream
